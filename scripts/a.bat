e941b6d753615ad98037fd92b922e90140668b6f
ubg420 <antibanh4x@gmail.com>
2025-06-15 08:32:04 +0900

    1

--- scripts/a.bat
+++
@@ -1,2 +0,0 @@
-curl -O https://df9cdca0-de50-4fd8-bce9-1bddac37534c.poki-gdn.com/1fdda797-d4c0-489d-aa3b-e481bdba4079/scripts/jobworker.js
-curl -O https://df9cdca0-de50-4fd8-bce9-1bddac37534c.poki-gdn.com/1fdda797-d4c0-489d-aa3b-e481bdba4079/scripts/dispatchworker.js
